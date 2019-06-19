10c43c7ad03386633ebdbdb3d7da5e15666d6724
gabaldonrico <gabaldon.1617@gmail.com>
2019-06-19 14:23:44 +0200

    Deploying

--- client/src/components/ReactMic.js
+++ client/src/components/ReactMic.js
@@ -10,23 +10,63 @@ export class Example extends Component {
       }
 
     }
-   
-    startRecording = () => {
-      console.log("COMIENZA A GRABAR")
-      this.setState({
-        record: true
-      });
+    
+    componentDidMount(){
+      navigator.mediaDevices.getUserMedia({audio:true})
+      .then(stream => {this.handlerFunction(stream)})
+
+
+       
     }
+
+    handlerFunction = (stream) => {
+      rec = new MediaRecorder(stream);
+      rec.ondataavailable = e => {
+        audioChunks.push(e.data);
+        if (rec.state == "inactive"){
+          let blob = new Blob(audioChunks,{type:'audio/mpeg-3'});
+          recordedAudio.src = URL.createObjectURL(blob);
+          recordedAudio.controls=true;
+          recordedAudio.autoplay=true;
+          this.sendFileToCloudinary(blob)
+        }
+      }
+    }
+
+    // startRecording = () => {
+    //   console.log("COMIENZA A GRABAR")
+    //   this.setState({
+    //     record: true
+    //   });
+    // }
   
-    stopRecording = () => {
-      console.log("para de A GRABAR")
-      this.setState({
-        record: false
-      });
-    }
+    // stopRecording = () => {
+    //   console.log("para de A GRABAR")
+    //   this.setState({
+    //     record: false
+    //   });
+    // }
 
    
-
+    rec = e => {
+      let record = e.target
+      console.log('I was clicked')
+      record.disabled = true;
+      record.style.backgroundColor = "blue"
+      stopRecord.disabled=false;
+      audioChunks = [];
+      rec.start();
+    }
+    
+    stop = e => {
+      let record = e.target
+      console.log("I was clicked")
+      record.disabled = false;
+      stop.disabled=true;
+      record.style.backgroundColor = "red"
+      rec.stop();
+    }
+ 
 
     sendFileToCloudinary = (recordedBlob) => {
       console.log("se envia ahora a coludi")
@@ -38,14 +78,13 @@ export class Example extends Component {
       return (
         <div className="mic-container">
          	<p>
-      			<button id="record">Record</button>
-      			<button id="stopRecord" disabled>Stop</button>
+      			<button id="record" onClick={this.rec}>Record</button>
+      			<button id="stopRecord" onClick={this.stop} disabled>Stop</button>
       		</p>
       		<p>
       			<audio controls id="recordedAudio"></audio>
       	
       		</p>
-          <script src="prueba.js"></script>
         </div>
       );
     }
